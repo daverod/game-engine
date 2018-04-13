ea3b650689575797c3d69d4775b1c09e027e5138
David Rodin <arodin@macbookpro.local>
2018-04-13 16:38:40 -0400

    Initial Commit

---
+++ Sparky/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sparky
+//
+//  Created by David Rodin on 4/13/18.
+//  Copyright © 2018 David Rodin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

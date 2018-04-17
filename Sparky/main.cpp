db3625d266d84671f687010deb4bc3ac4f3e68fa
David Rodin <arodin@MacBookPro.local>
2018-04-17 14:03:51 -0400

    input handling

--- Sparky/main.cpp
+++ Sparky/main.cpp
@@ -8,12 +8,12 @@
 
 #include <iostream>
 
-//GLEW
-#define GLEW_STATIC
-#include <GL/glew.h>
+////GLEW
+//#define GLEW_STATIC
+//#include <GL/glew.h>
 
 //GLFW
-#include <GLFW/glfw3.h>
+//#include <GLFW/glfw3.h>
 
 #include "src/graphics/window.hpp"
 
@@ -66,8 +66,26 @@ int main(int argc, const char * argv[]) {
     Window window("Sparky!", 800, 600);
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
     while (!window.closed()) {
         window.clear();
+        double x, y;
+        window.getMousePosition(x, y);
+        std::cout << x << " , " << y << std::endl;
+        if(window.isKeyPressed(GLFW_KEY_A))
+            std::cout << "a is pressed" << std::endl;
+        if(window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT))
+            std::cout << "mouse button left is pressed" << std::endl;
+//        std::cout << window.getHeight() << " , " << window.getWidth() << std::endl;
+        glBegin(GL_TRIANGLES);
+        glVertex2d(-0.5f, -0.5f);
+        glVertex2d( 0.0f,  0.5f);
+        glVertex2d( 0.5f,  -0.5f);
+        glEnd();
+        glDrawArrays(GL_ARRAY_BUFFER, 0, 0);
         window.update();
     }
     
